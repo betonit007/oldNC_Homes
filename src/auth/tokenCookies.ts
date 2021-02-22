aabd29dde602b3b5e8c6eaaf9df294b9fd5ef962
betonit007 <timnagorski@hotmail.com>
2021-02-22 13:46:42 -0500

    integrated firebase for authentication with useContext / AuthProvide

--- src/auth/tokenCookies.ts
+++ src/auth/tokenCookies.ts
@@ -1,3 +1,11 @@
-// import cookies from "js-cookie";
+import cookies from "js-cookie";
 
-export {};
+export const getTokenCookie = () => cookies.get('token')
+
+export const setTokenCookie = (token: string) => {
+    cookies.set("token", token, {
+        expires: 1 / 24 //one divied by 24 = 1 hour
+    })
+}
+
+export const removeTokenCookie = () => cookies.remove("token")
