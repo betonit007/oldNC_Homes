bbbd32d01222c79c01db8213d7c775c4ad6464b0
betonit007 <timnagorski@hotmail.com>
2021-02-25 13:03:02 -0500

    added searchbox to house form and search itself

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 // import { useRouter } from "next/router";
 // import Link from "next/link";
 // import { Image } from "cloudinary-react";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 // import {
 //   CreateHouseMutation,
 //   CreateHouseMutationVariables,
@@ -30,6 +30,8 @@ export default function HouseForm({ }: IProps) {
     const [submitting, setSubmitting] = useState(false)
     const { register, handleSubmit, setValue, errors, watch } = useForm<IFormData>({ defaultValues: {} })
 
+    const address = watch("address")
+
     useEffect(() => {
         register({ name: "address" }, { required: "Please enter  your house" })
         register({ name: "latitude" }, { required: true, min: -90, max: 90 })
@@ -49,8 +51,15 @@ export default function HouseForm({ }: IProps) {
 
             <div className="mt-4">
                 <label htmlFor="search" className='block'>Search for your address</label>
-
+                <SearchBox onSelectAddress={(address, latitude, longitude) => {
+                    setValue("address", address)
+                    setValue("latitude", latitude)
+                    setValue("longitude", longitude)
+                }}
+                    defaultValue=""
+                />
                 {errors.address && <p>{errors.address.message}</p>}
+                <h2>{address}</h2>
             </div>
         </form>
     )
